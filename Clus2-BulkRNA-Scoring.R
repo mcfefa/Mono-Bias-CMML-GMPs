06db04ec58d378aaccdbab852d7d09b5aed99391
Meghan Ferrall-Fairbanks <ferrallm@c0712a-s3.ufhpc>
2024-06-19 13:41:57 -0400

    successfully loaded TPM matrix

--- Clus2-BulkRNA-Scoring.R
+++ Clus2-BulkRNA-Scoring.R
@@ -12,10 +12,33 @@ libraryPath <- "/home/ferrallm/Mono-Bias-CMML-GMPs/lib2"
 library(DESeq2)
 library(tidyverse)
 library(tximport)
+library(BiocManger)
+# BiocManager::install("pasilla",lib.loc=libraryPath)
+# library('pasilla', lib.loc=libraryPath)
 
 ## code
-datadir <- "/blue/ferrallm/00_data/Moffitt-CICPT_4448_Padron_RNAseq-TRE/CITESeq_BulkRNASeq_Results_05312024/Salmon_Amy_CITESeq_BulkRNAseq/Filtered/"
+datadir <- "/blue/ferrallm/00_data/RNAseq/Moffitt-CICPT_4448_Padron_RNAseq-TRE/CITESeq_BulkRNASeq_Results_05312024/Salmon_Amy_CITESeq_BulkRNAseq/Filtered/"
+
+
+# Import data with pasilla following along with: 
+# bioconductor.org/packages/devel/bioc/vignettes/DESeq2/inst/doc/DECSeq2.html#count-matrix-input
 
 file <- paste(datadir,"Salmon_TPM_Filtered_GeneID_Data_without_Zeros.csv",sep="")
-bulkdata <- tximport(file, type="salmon", countsFromAbundance="scaledTPM", geneIDCol="GeneID")
+#bulkdata <- tximport(file, type="salmon", countsFromAbundance="scaledTPM", geneIDCol="GeneID")
 ## still troubleshooting reading in the file; waiting for more info from Surendra on scripts used to run salmon
+
+bulkcts <- as.matrix(read.csv(file,sep=",",row.names="GeneID"))
+head(bulkcts,2)
+
+## drop TPM_ in front of each sample name
+colnames(bulkcts) <- sub("TPM_","",colnames(bulkcts))
+head(bulkcts,4)
+
+BiocManger::install("GSVA")
+library(GSVA)
+## bioconductor.org/packages/devel/bioc/vignettes/GSVA/inst/doc/GSVA.html
+
+
+
+
+
